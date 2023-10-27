c7c415aec619fbeac3f018cc043660e547f08f37
TrungHieuTDC <21211TT0234@mail.tdc.edu.vn>
2023-10-28 02:56:23 +0800

    Initial commit

---
+++ PROJECT_ATM/PROJECT_ATM/User.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+    internal class User
+    {
+    //fields
+        private string id;
+        private string pin;
+    public int dem;
+
+    //constructor
+    public User(string id, string pin)
+    {
+        this.Id = id;
+        this.Pin = pin;
+        this.dem = 0;
+    }
+
+    public string Id
+    {
+        get
+        {
+            return id;
+        }
+
+        set
+        {
+            id = value;
+        }
+    }
+
+    public string Pin
+    {
+        get
+        {
+            return pin;
+        }
+
+        set
+        {
+            pin = value;
+        }
+    }
+
+    //properties
+
+    //method
+    public string toString()
+    {
+        return $"{Id, -15}{Pin,-15}";
+    }
+}
+
